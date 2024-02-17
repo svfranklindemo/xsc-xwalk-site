28e63ff17c46bf69375df145b85dfcfa3b99fa79
Deck Reyes <63816737+deckreyes@users.noreply.github.com>
2024-02-16 19:31:56 -0800

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -11,13 +11,9 @@ export default async function decorate(block) {
   const navPath = footerMeta ? new URL(footerMeta, window.location).pathname : (window.wknd.demoConfig.demoBase || '/footer');
   const footerPath = footerMeta ? new URL(footerMeta).pathname : '/footer';
 
-  if (${getSiteRoot(2)} = "/content") {
-    alert("first choice");  
-    const resp = await fetch(`${getSiteRoot(5)}${navPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
-    }else{
-      alert("second choice");  
-      const resp = await fetch(`${footerPath}.plain.html`);
-    }
+  const resp = await fetch(`${getSiteRoot(5)}${navPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+  /*const resp = await fetch(`${footerPath}.plain.html`);*/
+
   if (resp.ok) {
     block.textContent = '';
 
